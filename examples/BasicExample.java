254586204f335c67cf6eaac0947ece669229324e
JerryYang <20830326@qq.com>
2019-10-14 09:10:46 +0000

    Fix BasicExample to support variable engines and repeatable run

--- examples/BasicExample.java
+++ examples/BasicExample.java
@@ -1,9 +1,57 @@
 import io.pmem.pmemkv.Database;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class BasicExample {
+    private static final String PMEM_ROOT_PATH = "/dev/shm";
+    private static final int PMEM_SIZE = 1073741824 / 16;
     public static void main(String[] args) {
-        System.out.println("Starting engine");
-        Database db = new Database("vsmap", "{\"path\":\"/dev/shm\", \"size\":1073741824}");
+        String[] supportedEngines = new String [] {"cmap", "vsmap", "vcmap", "tree3", "stree"};
+        for (String engine : supportedEngines) {
+            System.out.println("Starting engine " + engine);
+            try {
+                runEngine(engine, generateConf(engine));
+            } catch (Exception e) {
+                System.out.println("Failed to test engine " + engine);
+                System.out.println(e);
+            }
+        }
+    }
+    private static String generateConf(String engine) {
+        boolean isVolatile = engine.equals("vsmap") || engine.equals("vcmap");
+        Path fullpath = Paths.get(PMEM_ROOT_PATH, engine);
+        StringBuilder confStr = new StringBuilder();
+        confStr.append("{\"path\":\"").append(fullpath.toString())
+                .append("\", \"size\":").append(PMEM_SIZE);
+        if (!isVolatile) {
+            confStr.append(", \"force_create\":1");
+        }
+        confStr.append("}");
+
+        if (Files.exists(fullpath)) {
+            if (!isVolatile) {
+                try {
+                    Files.delete(fullpath);
+                    System.out.println("Successfully cleaned " + fullpath.toString());
+                } catch (IOException ioe) {
+                    System.out.println("Failed to clean " + fullpath.toString() + ioe.getMessage());
+                }
+            }
+        } else if (isVolatile){
+            try {
+                Files.createDirectory(fullpath);
+            } catch (IOException ioe) {
+                System.out.println("Failed to create " + fullpath.toString() + ioe.getMessage());
+            }
+        }
+        return confStr.toString();
+    }
+    private static void runEngine(String engine, String confStr) {
+        System.out.println(String.format("Loading Engine %s @ %s", engine, confStr));
+        Database db = new Database(engine, confStr);
 
         System.out.println("Putting new key");
         db.put("key1", "value1");
