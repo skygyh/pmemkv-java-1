ec8cf6938e9b5e99d640c3a53bec8abe0c289914
gongy2 <yunhai.gong@emc.com>
2020-07-15 20:24:46 -0700

    update script

--- src/main/java/examples/run_example.sh
+++ src/main/java/examples/run_example.sh
@@ -5,5 +5,9 @@ then
 	echo "Please firstly run \"mvn compile\" at the root project path"
 	exit 1
 fi
+pushd `pwd`
+cd ../../../../
+mvn compile
+popd
 javac -cp .:${HERE}/../../../../target/classes/ BasicExample.java -d ${HERE}/../../../../target/classes/
 PMEM_IS_PMEM_FORCE=1 java -ea -Xms1G -cp ${HERE}/../../../../target/classes/ -Djava.library.path=/usr/local/lib examples.BasicExample
