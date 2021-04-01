e2121764db1e27ca6687032b94a58fdf598e5ac0
gongy2 <yunhai.gong@emc.com>
2021-03-31 19:27:16 -0700

    update script

--- src/main/java/examples/run_example.sh
+++ src/main/java/examples/run_example.sh
@@ -9,5 +9,9 @@ pushd `pwd`
 cd ../../../../
 mvn compile
 popd
+export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8 -Djava.library.path=/usr/local/lib"
+export  LD_LIBRARY_PATH=LD_LIBRARY_PATH:/usr/local/lib64
+export PMEMOBJ_CONF="sds.at_create=0"
+export PMEMBLK_CONF="sds.at_create=0"
 javac -cp .:${HERE}/../../../../target/classes/ BasicExample.java -d ${HERE}/../../../../target/classes/
 PMEM_IS_PMEM_FORCE=1 java -ea -Xms1G -cp ${HERE}/../../../../target/classes/ -Djava.library.path=/usr/local/lib examples.BasicExample
